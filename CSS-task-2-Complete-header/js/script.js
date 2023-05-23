2d854ee686512aefa0268c2231b8e9abe9aeec2e
krrish_dev <justbegintoday@gmail.com>
2023-05-23 13:17:26 +0300

    update bonse section flexbox

--- CSS-task-2-Complete-header/js/script.js
+++ CSS-task-2-Complete-header/js/script.js
@@ -1,40 +1,38 @@
-// jQuery code to handle the click event and toggle the dropdown menu
 $(document).ready(function() {
-    $('#accountDropdownButton').click(function() {
-        $('#accountDropdownMenu').toggle();
-      });
-
-    $('#currencyDropdownButton').click(function() {
-        $('#currencyDropdownMenu').toggle();
-      });
+    // Navbar dropdowns
+    $("#menu-drop-one").click(function() {
+    $(".nav-right .account .accounts").toggleClass("show");
+    });
+    $("#menu-drop-two").click(function() {
+    $(".nav-right .account .usd").toggleClass("show");
+    });
+    $("#menu-drop-three").click(function() {
+    $(".nav-right .account .en").toggleClass("show");
+    });
     
-      $('#languageDropdownButton').click(function() {
-        $('#languageDropdownMenu').toggle();
-      });
-
-      
-  });
-
-
-
-
-
-
-// $(document).ready(function() {
-//     // Toggle the dropdown menus on click
-//     $('.dropdown-toggle').click(function() {
-//       var menu = $(this).siblings('.dropdown-menu');
-//       $('.dropdown-menu').not(menu).removeClass('show');
-//       menu.toggleClass('show');
-//     });
-  
-//     // Hide the dropdown menus when clicking outside
-//     $(document).click(function(event) {
-//       var target = $(event.target);
-//       if (!target.closest('.dropdown').length) {
-//         $('.dropdown-menu').removeClass('show');
-//       }
-//     });
-//   });
-
- 
+    // Navbar links
+    $(".nav-bar .left .links li:not(.page)").click(function() {
+    $(this).siblings().removeClass("active");
+    $(this).addClass("active");
+    });
+    
+    // Navbar categories dropdown
+    $(".nav-bar .left .categories .cat").click(function() {
+    $(".nav-bar .left .categories .cat-dropDown").toggleClass("show");
+    });
+    
+    // Pages dropdown
+    $(".nav-bar .links .dropDown").click(function() {
+    $(".nav-bar .left .links ul .pages-drop").toggleClass("show");
+    });
+    
+    // Dresses menu
+    $(".nav-bar .left .categories .cat-dropDown .dress-drop").click(function() {
+    $(".nav-bar .left .categories .cat-dropDown .dressDown").toggleClass("show");
+    });
+    
+    // Mobile dropdown
+    $(".nav-bar .mobile-bars").click(function() {
+    $(".nav-bar .left .links").toggleClass("show");
+    });
+    });
