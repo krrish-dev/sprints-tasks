5126a4a4b56487baf687b1f55090a6d02e3bfc44
krrish_dev <justbegintoday@gmail.com>
2023-05-27 07:41:10 +0300

    JS Task 1 - Finding the Sum of Two Numbers and Fixing the String Prompt Issue

---
+++ JS-task-1/task.js
@@ -0,0 +1,4 @@
+function makeCalc(){
+    document.getElementById('result').innerHTML = 'The result of Sum : ' + (parseInt(document.getElementById('num1').value) + parseInt(document.getElementById('num2').value));
+    // alert('the result of Sum: ' + (parseInt(document.getElementById('num1').value) + parseInt(document.getElementById('num2').value)));
+}
