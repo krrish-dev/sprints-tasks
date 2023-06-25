5c2c0f2964510a6972624201541d0467956aef62
krrish_dev <justbegintoday@gmail.com>
2023-06-26 01:20:51 +0300

    update test user

--- Node-js-project/models/User.js
+++ Node-js-project/models/User.js
@@ -1,16 +1,16 @@
 // User model
-
+// P@asword  == hashed ==> b695b65072631ba885f63806c77db9325f9ae702f4f9e40743d1cb659e7d4070
 const users = [
   {
     id: 1,
-    email: 'krrish1@web.dev',
-    password: 'P@asword',
+    email: 'krrish@web.dev',
+    password: 'b695b65072631ba885f63806c77db9325f9ae702f4f9e40743d1cb659e7d4070',
     roles: ['products.read', 'products.list'],
   },
   {
     id: 2,
     email: 'test@test.com',
-    password: '12345',
+    password: 'b695b65072631ba885f63806c77db9325f9ae702f4f9e40743d1cb659e7d4070',
     roles: ['products.list'],
   },
 ];
